0dc096e96dd9ad58079ad05ab9bef895b5d7f8a2
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-01-03 12:53:26 -0300

    Add carousel navigation functionality for next and previous buttons

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ toggleBtns.forEach((toggleBtn, index) => {
             : 'fa-solid fa-bars';
     };
 });
+const nextDom = document.getElementById('next');
+const prevDom = document.getElementById('prev');
+const carouselDom = document.querySelector('.carousel');
+const ListItemDom = document.querySelector('.carousel .list');
+const thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+nextDom.onclick = function () {
+    showSlider('next');
+};
+
+function showSlider(type) {
+    const itemSlider = document.querySelectorAll('.carousel .list .item');
+    const thumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if (type === 'next') { // Corrigido: removido o espaço extra
+        ListItemDom.appendChild(itemSlider[0]); // Move o primeiro item para o final
+    } else if (type === 'prev') { // Implementação para o botão 'prev'
+        ListItemDom.insertBefore(itemSlider[itemSlider.length - 1], itemSlider[0]); // Move o último item para o início
+    }
+}
+
+prevDom.onclick = function () {
+    showSlider('prev'); // Adicionada funcionalidade para o botão 'prev'
+};
